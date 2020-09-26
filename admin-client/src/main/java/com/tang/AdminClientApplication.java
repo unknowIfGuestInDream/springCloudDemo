1572dd31e68b8e95021c13e900495e81f590c890
唐亮 <tang97155@163.com>
2020-09-26 11:45:16 +0800

    新增admin server监控
    Signed-off-by: 唐亮 <tang97155@163.com>

---
+++ admin-client/src/main/java/com/tang/AdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.tang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+
+}
---
+++ admin-client/src/main/java/com/tang/domain/User.java
@@ -0,0 +1,46 @@
+package com.tang.domain;
+
+/**
+ * @author: TangLiang
+ * @date: 2020/9/9 9:58
+ * @since: 1.0
+ */
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
